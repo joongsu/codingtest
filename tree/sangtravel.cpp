87e3e07336a0ff598fc5f0eb8f38786a0b812a62
joongsu <gjs4565@naver.com>
2024-09-27 19:54:08 +0900

    백준-9372 신장트리정의문제

---
+++ tree/sangtravel.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // 백준-9372 상근이의 여행
+                    // 신장트리의 정의에 관한 문제
+using namespace std;
+
+int main(){
+
+
+    int T;
+    cin >> T;
+
+    for(int i = 0 ; i < T ; i++){
+        int n,m;
+        cin >> n >> m;
+        for(int j=0; j < m ; j++){
+            int a,b;
+            cin >> a >> b;
+        }
+        cout << n-1 << '\n';
+    }
+}
