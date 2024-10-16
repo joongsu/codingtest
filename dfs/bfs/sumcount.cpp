9b9ddaccf3ef11f5d56eb065d387e9c409eda5d7
gojungsu <gjs4565@naver.com>
2024-10-16 17:01:41 +0900

    섬의 개수 - 4963

--- dfs/bfs/sumcount.cpp
+++ dfs/bfs/sumcount.cpp
@@ -3,23 +3,23 @@
 #include <vector>
 using namespace std;
 int graph[50][50];
-int visit[50][50];
+int visited[50][50];
 int dy[] = {-1,-1,0,1,1,1,0,-1}; // 12시부터 시계방향
 int dx[] = {0,1,1,1,0,-1,-1,-1};
-int count = 0;
+int counted = 0;
 int width;
 int height;
 vector<int> result;
 void dfs(int y,int x){
-    if(visit[y][x] == 1) return;
-    visit[y][x] = 1;
+    if(visited[y][x] == 1) return;
+    visited[y][x] = 1;
     for(int i = 0 ; i < 8 ; i++){
         int ny = dy[i] + y;
         int nx = dx[i] + x;
 
         if(ny < 0 || nx < 0 || nx >= width || ny >= height) continue;
 
-        if(visit[ny][nx] != 1 && graph[ny][nx] == 1){
+        if(visited[ny][nx] != 1 && graph[ny][nx] == 1){
             dfs(ny,nx);
         }
     }
@@ -46,17 +46,17 @@ int main(){
 
         for(int k = 0 ; k < h ; k++){
             for(int l = 0; l < w ; l++){
-                if(graph[k][l] == 1 && visit[k][l] != 1){
+                if(graph[k][l] == 1 && visited[k][l] != 1){
                     dfs(k,l);
-                    count++;
+                    counted++;
                 }
             }
         }
-        result.push_back(count);
-        count = 0;
+        result.push_back(counted);
+        counted = 0;
 
         memset(graph, 0, sizeof(graph));
-        memset(visit, 0, sizeof(visit));
+        memset(visited, 0, sizeof(visited));
     }
 
     for(auto &it : result)
