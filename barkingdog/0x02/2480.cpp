fddc591bbd3e564f4150b48cf35e873d8e39c1ea
gojungsu <gjs4565@naver.com>
2025-03-04 02:35:52 +0900

    2480 - 주사위 세개

---
+++ barkingdog/0x02/2480.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // 주사위 세개
+#include <algorithm>  
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a,b,c;
+    cin >> a >> b >> c;
+
+    if(a == b && b == c){
+        cout << (10000 + a * 1000);
+    }else if(a == b || b == c) cout << 1000 + b * 100;
+    else if(a == c) cout << 1000 + a*100;
+    else cout << max({a,b,c}) * 100;
+}
